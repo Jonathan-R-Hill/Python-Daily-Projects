8d9e439ced9f8fc03075663ebebc8e5c979d4e3f
Jonathan Hill <76394328+Jonathan-R-Hill@users.noreply.github.com>
2022-12-01 19:05:16 +0000

    Delete Day 3 (6 Projects basically if statements) directory

--- Day 3 (6 Projects basically if statements)/Leap year.py
+++
@@ -1,8 +0,0 @@
-print("Leap year checker!")
-year = int(input("What year do you want to check?\n"))
-
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print("It's a leap year")
-
-else:
-    print("It is not a leap year")
--- Day 3 (6 Projects basically if statements)/Odd or even.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Which number do you want to check?\n"))
-
-if num % 2 == 0:
-    print("Your number is Even")
-
-else:
-    print("Your number is Odd")
